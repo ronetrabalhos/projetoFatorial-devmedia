cc74f7b0a084733e088193719aad6f96cd80dab3
rone.trabalhos <rone.trabalhos@gmail.com>
2017-03-14 15:53:23 -0300

    criacao da primeira versao da classe vetorial

---
+++ Fatorial.java
@@ -0,0 +1,15 @@
+public class Fatorial{
+
+    public static void main (string [] args){
+        system.out.println(calcularFatorial(5));
+    }
+
+    public static int calcularFatorial(){
+        int resultado = 1;
+
+        for(int i = 2; i <= n; i++)
+            resultado* = i;
+        
+        return resultado;
+    }
+}
